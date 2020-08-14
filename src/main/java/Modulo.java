ae7301eb94c134db4d250d42610e6bbd93aa11e9
dirk <dirk@gmx.org>
2020-08-14 12:12:45 +0200

    Implement euclidean algorithm

--- src/main/java/Modulo.java
+++ src/main/java/Modulo.java
@@ -12,9 +12,21 @@ public class Modulo {
         return r;
     }
 
+    /**
+     * Uses the euclidean algorithm to calculate the
+     * greatest common divisor
+     */
+    public static int ggT( int a, int b ) {
+        if( b == 0  ||  a == b ) return a;
+        if( a > b ) return ggT( b, modulo(a,b) );
+        return ggT(b,a);
+    }
+
     public static void main(String[] args){
         System.out.println( "22 / 5 =  " + (22/4) );
         System.out.println( "22 % 5 =  " + modulo(22,4) );
         System.out.println( "-5 % 3 =  " + modulo(-5,3) );
+        System.out.println( "ggT(1020,768) = " + ggT(1020,768) );
+        System.out.println( "ggT(768,1020) = " + ggT(768,1020) );
     }
 }
