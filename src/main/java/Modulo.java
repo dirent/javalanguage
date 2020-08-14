f8d1d413326be69e8ea1b265a00bcd49c3d7cb19
dirk <dirk@gmx.org>
2020-08-14 10:47:52 +0200

    Add example of modulo operator

---
+++ src/main/java/Modulo.java
@@ -0,0 +1,8 @@
+public class Modulo {
+
+    public static void main(String[] args){
+        System.out.println( "22 / 5 =  " + (22/4) );
+        System.out.println( "22 % 5 =  " + (22%4) );
+        System.out.println( "-5 % 3 = " + (-5%3) );
+    }
+}
