1110763a53c95ad9a7de8f91d7d212c61981f761
dirk <dirk@gmx.org>
2020-08-14 11:09:03 +0200

    Calculate positive remainder

--- src/main/java/Modulo.java
+++ src/main/java/Modulo.java
@@ -1,8 +1,16 @@
 public class Modulo {
 
+    public static int modulo( int a, int b ) {
+        int r = a % b;
+        if( r < 0 ) {
+            return r+b;
+        }
+        return r;
+    }
+
     public static void main(String[] args){
         System.out.println( "22 / 5 =  " + (22/4) );
-        System.out.println( "22 % 5 =  " + (22%4) );
-        System.out.println( "-5 % 3 = " + (-5%3) );
+        System.out.println( "22 % 5 =  " + modulo(22,4) );
+        System.out.println( "-5 % 3 =  " + modulo(-5,3) );
     }
 }
